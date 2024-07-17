39904e2a3ea3c137e08a80cb20986668e79183fb
Considine Jr, Bobby <bconsidi@iu.edu>
2024-07-17 10:55:41 -0400

    finish

--- ValorantTracker/obj/Debug/net8.0/ValorantTracker.AssemblyInfo.cs
+++ ValorantTracker/obj/Debug/net8.0/ValorantTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValorantTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9df6287665e706419808ff069ac2bb1e072952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f835996b888fdb08f2be9b7211421c3348176aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValorantTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValorantTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
